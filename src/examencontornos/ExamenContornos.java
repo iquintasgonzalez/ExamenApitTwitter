e00f34e43b1d160c7e5ec961e236c5b188324e58
iquintasgonzalez <iquintasgonzalez@eniac06.danielcastelao.org>
2015-03-16 09:07:34 +0100

    poner las acces token en el main

--- src/examencontornos/ExamenContornos.java
+++ src/examencontornos/ExamenContornos.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 import javax.swing.JOptionPane;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
 /**
  *
@@ -14,6 +16,16 @@ public class ExamenContornos {
     public static void main(String[] args) throws TwitterException {
         // variable declarada
         Twitter twitter;
+        
+          ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey("0AOUWd7NMNgCi11E1mqpKCkuL")
+                .setOAuthConsumerSecret("xW2KlbmdUaKNnBSWm73p9VpL9ywNeGrx726tXVv7R4Wji8a0KK")
+                .setOAuthAccessToken("3055142195-sx2xtJqgPrjUXrP7Dtd8YuRHdzH6GsOMSHIG4nq")
+                .setOAuthAccessTokenSecret("gZekOzP1RBvqbOwg2zUHhVy4OcMSdub8YIeRTXGqGGVG7");
+
+        twitter = new TwitterFactory(cb.build()).getInstance();
+
         //variables declaradas
         String busqueda;
         String tweet;

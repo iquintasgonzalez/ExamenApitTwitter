1f1628a275655ead60fc5bcc03cf6d993d422b91
iquintasgonzalez <iquintasgonzalez@eniac06.danielcastelao.org>
2015-03-16 08:50:00 +0100

    menu creado en el main

--- src/examencontornos/ExamenContornos.java
+++ src/examencontornos/ExamenContornos.java
@@ -1,6 +1,11 @@
 
 package examencontornos;
 
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+
 /**
  *
  * @author iquintasgonzalez
@@ -8,8 +13,55 @@ package examencontornos;
 public class ExamenContornos {
 
     
-    public static void main(String[] args) {
-       
+    public static void main(String[] args) throws TwitterException {
+      
+        Twitter twitter;
+
+   String busqueda;
+        String tweet;
+        String nombre;
+        String mensaje;
+        int opcion;
+
+        Metodos obj = new Metodos();
+
+        obj.verTL();
+
+        busqueda = JOptionPane.showInputDialog("Introduzca lo que desee buscar en Twitter");
+        obj.buscarTrending(busqueda);
+
+        tweet = JOptionPane.showInputDialog("Introduzca lo que desee twittear");
+        obj.tweetear(tweet);
+
+        nombre = JOptionPane.showInputDialog("Usuario");
+        mensaje = JOptionPane.showInputDialog("¿Que mensaje le quieres enviar?");
+        obj.directMessage(nombre, mensaje);
+
+        do {
+            opcion = Integer.parseInt(JOptionPane.showInputDialog("opciones: \n1 --> TimeLine\n2 --> Buscar\n3 --> Tweettear\n4 --> DirectMsg\n5 --> Exit"));
+            switch (opcion) {
+                case 1:
+                    obj.verTL();
+                    break;
+                case 2:
+                    busqueda = JOptionPane.showInputDialog("Introduzca lo que desee buscar");
+                    obj.buscarTrending(busqueda);
+                    break;
+                case 3:
+                    tweet = JOptionPane.showInputDialog("Introduzca lo que desee twittear");
+                    obj.tweetear(tweet);
+                    break;
+                case 4:
+                    nombre = JOptionPane.showInputDialog("Usuario");
+                    mensaje = JOptionPane.showInputDialog("¿Que mensaje le quieres enviar?");
+                    obj.directMessage(nombre, mensaje);
+                    break;
+                case 5:
+                    System.exit(0);
+                default:
+                    System.exit(0);
+            }
+        } while (opcion != 0 && opcion != 4);
     }
-    
 }
+
